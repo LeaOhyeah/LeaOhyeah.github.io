35cd34ffb63a556376fc675a71308fd2ba8706aa
LeaOhyah <leasehat@gmail.com>
2022-10-13 15:56:34 +0700

    merapikan

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,31 @@
-var multipleCardCarousel = document.querySelector(
-     "#carouselExampleControls"
+var sliderCard = document.querySelector(
+     "#sliderCard"
    );
    if (window.matchMedia("(min-width: 768px)").matches) {
-     var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+     var carousel = new bootstrap.Carousel(sliderCard, {
        interval: false,
      });
-     var carouselWidth = $(".carousel-inner-cs")[0].scrollWidth;
-     var cardWidth = $(".carousel-item-cs").width();
+     var inner = $(".carousel-inner-cs")[0].scrollWidth;
+     var item = $(".carousel-item-cs").width();
      var scrollPosition = 0;
-     $("#carouselExampleControls .carousel-control-next-cs").on("click", function () {
-       if (scrollPosition < carouselWidth - cardWidth * 4) {
-         scrollPosition += cardWidth;
-         $("#carouselExampleControls .carousel-inner-cs").animate(
+     $("#sliderCard .carousel-control-next-cs").on("click", function () {
+       if (scrollPosition < inner - item * 4) {
+         scrollPosition += item;
+         $("#sliderCard .carousel-inner-cs").animate(
            { scrollLeft: scrollPosition },
            900
          );
        }
      });
-     $("#carouselExampleControls .carousel-control-prev-cs").on("click", function () {
+     $("#sliderCard .carousel-control-prev-cs").on("click", function () {
        if (scrollPosition > 0) {
-         scrollPosition -= cardWidth;
-         $("#carouselExampleControls .carousel-inner-cs").animate(
+         scrollPosition -= item;
+         $("#sliderCard .carousel-inner-cs").animate(
            { scrollLeft: scrollPosition },
            900
          );
        }
      });
    } else {
-     $(multipleCardCarousel).addClass("slide");
+     $(sliderCard).addClass("slide");
    }
